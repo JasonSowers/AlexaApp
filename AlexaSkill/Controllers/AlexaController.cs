fce4ddf5629886e538377617c632df2b801ad6e5
Jason Sowers <jason.sowers@symetra.com>
2017-04-25 04:24:53 -0700

    initial commit

---
+++ AlexaSkill/Controllers/AlexaController.cs
@@ -0,0 +1,53 @@
+﻿using System.Web.Http;
+using AlexaPOCDataModel;
+
+namespace AlexaSkill.Controllers
+{
+    
+    public class AlexaController : ApiController
+    {
+        [HttpPost, Route("api/alexa/demo")]
+        public dynamic nachos(AlexaRequest alexaRequest)
+        {
+            //var request = 
+                new Requests().Create(new Request
+                {
+                MemberId = (alexaRequest.Session.Attributes == null) ? 0 : alexaRequest.Session.Attributes.MemberId,
+                Timestamp = alexaRequest.Request.Timestamp,
+                Intent = (alexaRequest.Request.Intent == null) ? "" : alexaRequest.Request.Intent.Name,
+                AppId = alexaRequest.Session.Application.ApplicationId,
+                RequestId = alexaRequest.Request.RequestId,
+                SessionId = alexaRequest.Session.SessionId,
+                UserId = alexaRequest.Session.User.UserId,
+                IsNew = alexaRequest.Session.New,
+                Version = alexaRequest.Version,
+                Type = alexaRequest.Request.Type,
+                Reason = alexaRequest.Request.Reason,                
+                DateCreated = System.DateTime.UtcNow
+            });
+            return new
+            {
+
+                version = "1.0",
+                sessionAttributes = new { },
+                response = new
+                {
+                    outputSpeech = new
+                    {
+                        type = "PlainText",
+                        text = "Hello World"
+                    },
+                    card = new
+                    {
+                        type = "Simple",
+                        title = "Pliralsight",
+                        content = "Hello\ncruel world!"
+                    },
+                    shouldEndSession = true
+                }
+
+               
+            };
+        }
+    }
+}
